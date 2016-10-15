10d35b37e1c78a38f48d4c68429537c2b2e935e2
chclau <chclau@yahoo.com>
2016-10-15 19:22:17 +0300

    Add files via upload

--- tb/tb_sat_cnt.vhd
+++ tb/tb_sat_cnt.vhd
@@ -16,17 +16,17 @@ end entity;
  
 architecture test of tb_sat_cnt is
 
-   constant DATA_W     : natural := 8                       ; 
-   constant CLK_FREQ    : natural := 80                                 ; -- in MHz
-   constant CLK_PER     : natural := 1000000 / CLK_FREQ     ; -- in ps
+   constant DATA_W      : natural := 8                         ; 
+   constant CLK_FREQ    : natural := 80                        ; -- in MHz
+   constant CLK_PER     : natural := 1000000 / CLK_FREQ        ; -- in ps
    constant clk_period  : time := CLK_PER * 1 ps               ;
    
-   signal clk      : std_logic := '0';
-   signal rst      : std_logic := '1';
-   signal en       : std_logic := '0';
-   signal sclr     : std_logic := '0';
+   signal clk        : std_logic := '0';
+   signal rst        : std_logic := '1';
+   signal en         : std_logic := '0';
+   signal sclr       : std_logic := '0';
    
-   signal endSim    : boolean   := false;
+   signal endSim     : boolean   := false;
 
    component sat_cnt  is
       generic (
@@ -34,11 +34,11 @@ architecture test of tb_sat_cnt is
          CNT_MAX     : natural := 20
       );
       port (
-         clk:     in std_logic;
-         rst:     in std_logic;
+         clk:        in std_logic;
+         rst:        in std_logic;
          
          -- inputs
-         sclr:    in std_logic;
+         sclr:       in std_logic;
          en:         in std_logic;
          
          data_out:   out std_logic_vector(DATA_W-1 downto 0)
