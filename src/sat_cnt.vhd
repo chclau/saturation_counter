01238a66fecac1e9f868b1f825a992b79b721b7f
chclau <chclau@yahoo.com>
2016-10-15 19:13:58 +0300

    Add files via upload

--- src/sat_cnt.vhd
+++ src/sat_cnt.vhd
@@ -3,21 +3,20 @@
 -- Description : Counter with saturation
 -- Designed by : Claudio Avi Chami - FPGA Site
 --               http://fpgasite.net
--- Date        : 04/October/2016
--- Version     : 01
+-- Date        : 15/October/2016
 ------------------------------------------------------------------
 library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 
 entity sat_cnt is
-  generic (
-    DATA_W      : natural := 32;
-    CNT_MAX     : natural := 20
-  );
-  port (
-    clk:        in std_logic;
-    rst:        in std_logic;
+   generic (
+      DATA_W      : natural := 32;
+      CNT_MAX     : natural := 20
+   );
+   port (
+      clk:        in std_logic;
+      rst:        in std_logic;
 		
     -- inputs
     sclr:       in std_logic;
@@ -35,17 +34,17 @@ begin
 max      <= '1' when cnt = to_unsigned(CNT_MAX-1, cnt'length) else '0';
 data_out <= std_logic_vector(cnt);
 
-  counter_pr: process (clk, rst) 
-  begin 
-    if (rst = '1') then 
-      cnt <= (others => '0');
-    elsif (rising_edge(clk)) then			
-	  if (sclr = '1') then                  -- sync clear
-        cnt <= (others => '0');
-      elsif (en = '1' and max = '0') then   -- enabled and maximum not reached?
-        cnt <= cnt + 1;                     -- increment counter
-      end if;	
-    end if;
-  end process;
+   counter_pr: process (clk, rst) 
+   begin 
+      if (rst = '1') then 
+         cnt <= (others => '0');
+      elsif (rising_edge(clk)) then			
+         if (sclr = '1') then                  -- sync clear
+            cnt <= (others => '0');
+         elsif (en = '1' and max = '0') then   -- enabled and maximum not reached?
+            cnt <= cnt + 1;                    -- increment counter
+         end if;	
+      end if;
+   end process;
 
 end rtl;
