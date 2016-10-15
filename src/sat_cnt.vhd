11e4bd470c3bb1d4d0973a768c1f94bbdd44d7fe
chclau <chclau@yahoo.com>
2016-10-15 19:22:38 +0300

    Add files via upload

--- src/sat_cnt.vhd
+++ src/sat_cnt.vhd
@@ -6,8 +6,8 @@
 -- Date        : 15/October/2016
 ------------------------------------------------------------------
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std.all;
 
 entity sat_cnt is
    generic (
@@ -17,18 +17,18 @@ entity sat_cnt is
    port (
       clk:        in std_logic;
       rst:        in std_logic;
-		
-    -- inputs
-    sclr:       in std_logic;
-    en:         in std_logic;
-		
-    data_out:   out std_logic_vector(DATA_W-1 downto 0)
+      
+      -- inputs
+      sclr:       in std_logic;
+      en:         in std_logic;
+      
+      data_out:   out std_logic_vector(DATA_W-1 downto 0)
   );
 end sat_cnt;
 
 architecture rtl of sat_cnt is
-	signal	cnt : unsigned(DATA_W-1 downto 0);
-	signal	max : std_logic;
+   signal   cnt : unsigned(DATA_W-1 downto 0);
+   signal   max : std_logic;
 
 begin 
 max      <= '1' when cnt = to_unsigned(CNT_MAX-1, cnt'length) else '0';
@@ -38,12 +38,12 @@ data_out <= std_logic_vector(cnt);
    begin 
       if (rst = '1') then 
          cnt <= (others => '0');
-      elsif (rising_edge(clk)) then			
+      elsif (rising_edge(clk)) then       
          if (sclr = '1') then                  -- sync clear
             cnt <= (others => '0');
          elsif (en = '1' and max = '0') then   -- enabled and maximum not reached?
             cnt <= cnt + 1;                    -- increment counter
-         end if;	
+         end if;  
       end if;
    end process;
 
